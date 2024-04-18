2f1954b0a1ac7a01bfcd0e423d666d270184cb45
NimeshChanuka2000 <nimeshchanu1234@gmail.com>
2024-04-18 14:42:35 +0530

    initial commit

---
+++ src/main/java/lk/chanu/OAuth2/OAuth2IntegrateExampleApplication.java
@@ -0,0 +1,13 @@
+package lk.chanu.OAuth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2IntegrateExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuth2IntegrateExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/chanu/OAuth2/OAuth2IntegrateExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.chanu.OAuth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2IntegrateExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
